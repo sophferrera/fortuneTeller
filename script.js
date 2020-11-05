09b5486705b99f4c61e3d71efeff1006ba1f27d2
Sophia Ferrera <slf445@nyu.edu>
2020-11-05 17:44:12 -0500

    made it nicer

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ function halfNumber() {
 fortuneButton.addEventListener("click", fortune);
 
 function fortune() {
-  var fortunes = ["There's a She Wolf in the closet. Open up and set her free.", "Sometimes love comes around and it knocks you down. Just get back up when it knocks you down, knocks you down", "Just dance, gonna be okay, da da doo-doo-mmm. Just dance, spin that record, babe, da da doo-doo-mmm", "You a mom and pop, I'm a corporation, I'm the press conference, you're a conversation ah ooh oh oh"]
+  var fortunes = ["You will become a substitute teacher for a fourth grade class and teach them how to rock", "You will elaborately dress like a nanny to spend more time with your children after your messy divorce", "You will meet your twin at summer camp and decide to trade places so you can get your parents back together", "You will go to France to look for, find, and kill your daughters kidnappers", "You will become an FBI agent and aid in capturing of Buffalo Bill", "You will become the founder of the most popular social media platform ever created but everyone hates you and you have no friends", "You will fall in love with your AI with Scarlett Johansson's voice but realize polyamory is not for you", "You will be cast as the swan queen but you will let Mila Kunis get to your head so you die"]
   var randomFortune = Math.floor(Math.random() * fortunes.length)
   var fortuneInputText = fortuneInput.value;
   fortuneOutput.innerHTML = fortuneInputText + ": " + fortunes[randomFortune];
 
 }
 
-function relstyle() {
+function restyle() {
   fortuneOutput.style.color = ("#FAB60D");
   fortuneButton.style.backgroundColor = ("#CC442E");
   halfButton.style.backgroundColor = ("#CC442E");
@@ -31,4 +31,4 @@ function relstyle() {
 
 }
 
-relstyle();
+restyle();
